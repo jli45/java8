ace33e9b7c1a22a953799125aae60edf2f22abb8
quan <Jing Li>
2016-01-28 22:59:16 -0800

    WIP

--- java8/src/java8/ThreadTest0.java
+++ java8/src/java8/ThreadTest0.java
@@ -5,12 +5,12 @@ class ThreadA extends Thread
 
 	@Override
 	public void run() {
-//		long start0 = System.currentTimeMillis();
-//		for(;;)
-//		{
-//			if (System.currentTimeMillis() -start0 > 100)
-//				break;
-//		}
+		//		long start0 = System.currentTimeMillis();
+		//		for(;;)
+		//		{
+		//			if (System.currentTimeMillis() -start0 > 100)
+		//				break;
+		//		}
 		// TODO Auto-generated method stub
 		for (int i =0;i<10;i++)
 		{
@@ -51,8 +51,15 @@ public class ThreadTest0 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		new ThreadA().start();
-		new ThreadB().start();
+		// This seems to always print A first, because "new ThreadB()" call in the main thread loses CPU 
+		// new ThreadA().start();
+		// new ThreadB().start();
+		
+		// This occasionally print B first, because OS schedules threads in an unpredictable way.
+		ThreadA A = new ThreadA();
+		ThreadB B = new ThreadB();
+		A.start();
+		B.start();
 
 	}
 
